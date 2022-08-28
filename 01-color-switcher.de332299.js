fcd418a6a26987f6a5355bcdc72942b7f5437092
Vanianollay <98497325+Vanianollay@users.noreply.github.com>
2022-08-28 18:46:04 +0000

    Deploying to gh-pages from @ Vanianollay/goit-js-hw-09@2fd167717b3895032c9bd8b445ad8847608a1773 🚀

---
+++ 01-color-switcher.de332299.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=0;e.setAttribute("disabled",""),t.addEventListener("click",(function(o){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled","")})),e.addEventListener("click",(function(o){clearInterval(n),t.removeAttribute("disabled",""),e.setAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.de332299.js.map
